cd4c3aeb0f384fefc3061115b2ac24008243e473
choeyujin <barbby20@naver.com>
2018-10-26 21:07:41 -0400

    extract text from image

---
+++ OCR/ocr.py
@@ -0,0 +1,20 @@
+import os
+import tempfile
+import subprocess
+
+def ocr(path):
+    temp = tempfile.NamedTemporaryFile(delete=False)
+
+    process = subprocess.Popen(['tesseract', path, temp.name], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    process.communicate()
+
+    with open(temp.name + '.txt', 'r') as handle:
+        contents = handle.read()
+
+    os.remove(temp.name + '.txt')
+    os.remove(temp.name)
+
+    return contents
+
+str = ocr('path/to/image')
+print(str)
