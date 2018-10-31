d17d6caf02a1ba2321ba38e5d792fc163f7b8f7d
lufovic77 <lufovic77@gmail.com>
2018-10-31 11:14:31 -0400

    contour process

--- ColorDetection/oulining.py
+++ ColorDetection/oulining.py
@@ -22,38 +22,43 @@ name = namewithformat.split(".")
 justName = name[0] #like "solar"
 
 #load images from disk 
-gwash = cv2.imread(args["image"])
-gwashBW = cv2.cvtColor(gwash, cv2.COLOR_BGR2GRAY)
+srcimg = cv2.imread(args["image"])
+grayimg = cv2.cvtColor(srcimg, cv2.COLOR_BGR2GRAY)
 
 filepath = "result/"+justName+"_grayscale.png" #jpg is not supported!!!!
-plt.imshow(gwashBW, 'gray')	
+plt.imshow(grayimg, 'gray')	
 plt.xticks([]), plt.yticks([])
 plt.savefig(filepath)	# how to save image using ndarray
-#plt.show() #shows the gray scale of pic
 
 #cv2.imshow('gwash', gwashBW) #Also supports conversion to the gray scale
 #cv2.waitKey(0)
 
-'''
-ret, thresh1 = cv2.threshold(gwashBW, 150, 255, cv2.THRESH_BINARY)
+ret, thresh = cv2.threshold(grayimg, 120, 255, cv2.THRESH_BINARY)
 #Parameters: (grayscale img src, threshold, applied value when exceeds the threshold, thresholding type)
 #WHITE when bigger than the threshold
 #BLACK when smaller than the threshold
+
+retimg, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+#several parameters: 1)src image, 2)contour retrieval mode, 3)contour approximation method 
+#output: 1)modified image, 2)contours(list of all contours), 3)hierarchy
+
+cnt = contours[4]
+cv2.drawContours(retimg, [cnt], 0, (0, 255, 0), 3)
+cv2.imshow("result", retimg)
+cv2.waitKey(0)
+
+'''
 kernel = np.ones((5, 5), np.uint8)
 erosion = cv2.erode(thresh1, kernel,iterations = 1)
 
 opening = cv2.morphologyEx(erosion, cv2.MORPH_OPEN, kernel)
 closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
 
-
 plt.imshow(closing, 'gray')
 plt.xticks([]), plt.yticks([])
 plt.show()
-'''
-'''
+
+
 contours, hierarchy = cv2.findContours(closing,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE) #find contours with simple approximation
 
-cv2.imshow('cleaner', closing) #Figure 3
-cv2.drawContours(closing, contours, -1, (255, 255, 255), 4)
-cv2.waitKey(0)
 '''
